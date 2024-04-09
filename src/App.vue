966a3583e39cf1ffb5a0d2a36592101204330172
Nicolas Chabloz <43348620+Chabloz@users.noreply.github.com>
2024-04-09 08:30:03 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+  import AppTemperature from './components/AppTemperature.vue';
+
+  import { computed, ref } from 'vue';
+
+  const tempSI = ref(10);
+
+  const tempCelsius = computed(() => {
+    return tempSI.value - 273.15;
+  })
+
+  const tempFaranheit = computed(() => {
+    return tempSI.value * 9/5 - 459.67;
+  });
+
+</script>
+
+<template>
+  <AppTemperature :temperature="tempCelsius" unit="°C"/>
+  <AppTemperature :temperature="tempFaranheit" unit="°F"/>
+  <AppTemperature :temperature="tempSI" unit="K"/>
+
+  <span>Hello</span>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/AppTemperature.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+  defineProps({
+    temperature: Number,
+    unit: String,
+  });
+
+</script>
+
+<template>
+  <div>
+    <span class="value">{{ temperature }} </span>
+    <span class="unit">{{ unit }}</span>
+  </div>
+</template>
+
+<style scoped>
+  span {
+    font-size: 2em;
+  }
+  span.value {
+    font-weight: bold;
+  }
+  span.unit::after {
+    content: "]";
+  }
+  span.unit::before {
+    content: "[";
+  }
+</style>
